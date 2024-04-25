3e6f731fcf54c3af1ae67fb0be9f03604ef20c54
Abdul <abdul@clearlyinnovative.com>
2024-04-24 18:00:57 -0600

    Add logos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import  ProfileCard  from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg-slate-400 rounded-xl p-8 border-solid border-stone-300">
+        <h1> Personal Data Aggregation System</h1>
+      </div>
+      <div className="bg-slate-400 rounded-xl p-8 border-solid border-stone-300">
+        <ProfileCard title='Alexa' handle='@alexa99' image={AlexaImage}/>
+        <ProfileCard title='Cortana' handle='@cortana32' image={CortanaImage}/>
+        <ProfileCard title='Siri' handle='@siri01' image={SiriImage}/>
+      </div>
     </div>
   );
 }
